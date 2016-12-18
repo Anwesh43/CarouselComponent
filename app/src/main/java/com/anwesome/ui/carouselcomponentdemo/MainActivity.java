4c23198c6189646353d7b87ade6e39abe0b2f12c
Anwesh43 <anweshthecool0@gmail.com>
2016-12-18 17:47:04 +0530

    Changing carousel images

--- app/src/main/java/com/anwesome/ui/carouselcomponentdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/carouselcomponentdemo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.anwesome.ui.carouselcomponentdemo;
 
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.anwesome.ui.customcarousel.CarouselComponent;
 import com.anwesome.ui.customcarousel.CarouselObject;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +20,24 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_main);
 
         CarouselComponent carouselComponent = new CarouselComponent(this);
-        carouselComponent.addCarousel(new CarouselObject(BitmapFactory.decodeResource(getResources(),R.drawable.sc1),"First Component"));
-        carouselComponent.addCarousel(new CarouselObject(BitmapFactory.decodeResource(getResources(),R.drawable.sc2),"Second Component"));
-        carouselComponent.addCarousel(new CarouselObject(BitmapFactory.decodeResource(getResources(),R.drawable.sc3),"Third Component"));
-        carouselComponent.addCarousel(new CarouselObject(BitmapFactory.decodeResource(getResources(),R.drawable.sc4),"Fourth Component"));
+        String titles[] = {"First Component","Second Component","Third Component","Fourth Component"};
+        Bitmap bitmaps[] = {BitmapFactory.decodeResource(getResources(),R.drawable.car1),BitmapFactory.decodeResource(getResources(),R.drawable.car2),BitmapFactory.decodeResource(getResources(),R.drawable.car3),BitmapFactory.decodeResource(getResources(),R.drawable.car4)};
+        addCarousels(carouselComponent,bitmaps,titles);
         carouselComponent.create();
+    }
+    private void addCarousels(CarouselComponent carouselComponent,Bitmap[] bitmaps,String[] titles) {
+        for(int i=0;i<titles.length;i++) {
+
+            Bitmap bitmap = bitmaps[i];
+            final String title = titles[i];
+            CarouselObject carouselObject = new CarouselObject(bitmap,title);
+            carouselObject.setOnClickListener(new ImageView.OnClickListener(){
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this,"Clicked on "+title,Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            carouselComponent.addCarousel(carouselObject);
+        }
     }
 }
